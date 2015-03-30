3339ed16d8f9eaca916368da689e0c8b6dc37b67
Shakna Israel <s4b3r6@gmail.com>
2015-03-30 10:46:06 +0000

    Generated Documentation

--- docbuilder.py
+++ docbuilder.py
@@ -1,12 +1,29 @@
+#!/usr/bin/env python
+
 import os
 import sys
 
 FILE = "write-good.py"
+EXPORT = "docs/index.md"
 string = "Unset"
+if os.path.isfile(EXPORT):
+    os.remove(EXPORT)
 file = open(FILE, "r")
+outfile = open(EXPORT, "w+")
 for line in file.read().split('\n'):
-    line = string
-    print "original string: ", string
-    string = string[1:]
-    print "string: " , string
+    string = line
+    string = string.strip()
+    char = string[:1]
+    if char == u'\u0023':
+        string = line[1:]
+        outfile.write("\n")
+        outfile.write(string)
+        outfile.write("\n")
+    else:
+        outfile.write("\n")
+        outfile.write("```")
+        outfile.write(string)
+        outfile.write("```")
+        outfile.write("\n")
 file.close()
+outfile.close()
