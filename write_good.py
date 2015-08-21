119c4f2e659ee35b00519827b302dce88056194f
James Milne <s4b3r6@gmail.com>
2015-08-22 02:11:08 +1000

    Given no options, print help

--- write_good.py
+++ write_good.py
@@ -1,9 +1,13 @@
 import argparse
+import sys
 
 def get_cli_flags():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--input", help="The input file. Hopefully plain text.")
     parser.add_argument("-o", "--output", help="An output file to generate.")
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
     cliArgs = parser.parse_args()
     if cliArgs.input:
         inFile = cliArgs.input
